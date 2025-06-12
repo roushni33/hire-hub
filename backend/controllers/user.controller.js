08376c1442d4b6ee3293e0a874c5016a58980e69
roushni33 <lovelyroushni33@gmail.com>
2025-06-12 18:23:52 +0530

    fix: update SameSite attribute for login cookie to 'None' and ensure secure flag is set

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -70,7 +70,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
     return res
         .status(200)
-        .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' })
+        .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'None', secure: true, })
         .json(
             new ApiResponse(200, { loginUser }, `Welcome back ${user.fullName}.`
 
